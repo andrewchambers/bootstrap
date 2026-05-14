bf64accc91505f4b3ba0266ce0835c11c73acc4d
Andrew Chambers <andrewchambers@fastmail.com>
2026-05-14 22:24:31 +1200

    Update tinybinutils and minias pins

--- bootstrap.sh
+++ bootstrap.sh
@@ -156,7 +156,8 @@ prepare_sources() {
 	prepare_git mcpp https://github.com/museoa/mcpp.git 2.7.2.2
 	prepare_git oksh https://github.com/ibara/oksh.git oksh-7.8
 	prepare_git pigz https://github.com/madler/pigz.git fe4894f57739e3039a2ffc2a2a360d35e19bacbe
-	prepare_git tinybinutils https://github.com/andrewchambers/tinybinutils.git 139b38c3990ab4513448cd5b9fc3bc3c6c7ea7fe
+	prepare_git tinybinutils https://github.com/andrewchambers/tinybinutils.git 98cd2a564696f7b974fe8d2f8d656aadd1ae5f32
+	prepare_git minias https://github.com/andrewchambers/minias.git ae25f889dd75d0616a5ae5b16a5253971c42393d
 	prepare_git sbase https://git.suckless.org/sbase c1341583c96307cb0e6152c963ed23c4d56a4278
 
 	prepare_tar xz-5.8.1.tar.gz https://tukaani.org/xz/xz-5.8.1.tar.gz \
@@ -406,14 +407,34 @@ build_tinybinutils() {
 			CC="$CC_FOR" \
 			CFLAGS="$CFLAGS_FOR" \
 			LDFLAGS="$LDFLAGS_FOR" \
-			tinyld tinyas tinyar
+			tinyld tinyar
 		mkdir -p "$out/bin"
-		cp tinyld tinyas tinyar "$out/bin/"
+		cp tinyld tinyar "$out/bin/"
 		ln -sf tinyld "$out/bin/ld"
-		ln -sf tinyas "$out/bin/as"
 		ln -sf tinyar "$out/bin/ar"
 		printf '%s\n' "#!$shell_path" 'exit 0' > "$out/bin/ranlib"
 		chmod +x "$out/bin/ranlib"
+	)
+}
+
+build_minias() {
+	prev=$1
+	out=$2
+	dir=$BUILD/minias
+	log "build minias"
+	copy_source minias "$dir"
+	(
+		cd "$dir"
+		set_stage_env "$prev" "$out"
+		"$MAKE_CMD" clean
+		"$MAKE_CMD" -j"$JOBS" \
+			CC="$CC_FOR" \
+			CFLAGS="$CFLAGS_FOR" \
+			LDFLAGS="$LDFLAGS_FOR" \
+			minias
+		mkdir -p "$out/bin"
+		cp minias "$out/bin/"
+		ln -sf minias "$out/bin/as"
 	)
 }
 
@@ -590,6 +611,7 @@ build_stage() {
 	build_mawk "$prev" "$out" "$prefix"
 	build_zlib "$prev" "$out" "$prefix"
 	build_tinybinutils "$prev" "$out" "$prefix"
+	build_minias "$prev" "$out" "$prefix"
 	build_pigz "$prev" "$out" "$prefix"
 	build_xz "$prev" "$out" "$prefix"
 	build_oksh "$prev" "$out" "$prefix"
